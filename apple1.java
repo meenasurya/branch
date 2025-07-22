b3314905cab230259f85b666cba9496c50c4c359
MeenasuryaAutomationTester <meenasurya@gmail.com>
2025-07-22 12:41:27 +0530

    ji

---
+++ apple1.java
@@ -0,0 +1,7 @@
+public class apple
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hi vijju");
+	}
+}
---
+++ apple2.java
@@ -0,0 +1,7 @@
+public class apple2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("ok");
+	}
+}
